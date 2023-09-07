0526e01e8934440a94eb4d8650bce8e055f4c989
George <gogoayman767@gmail.com>
2023-09-07 15:52:24 -0700

    0-hello_world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,11 @@
  * Return: 0 (success)
 */
 int main(void)
-}
-	printf("Size of a char : %d buyte(s) \n ", sizeof(char));
-	printf("Size of an int : %d buyte(s) \n ", sizeof(int));
-	printf("Size of a float : %d buyte(s) \n ", sizeof(float));
-	printf("Size of a long int : %d buyte(s) \n ", sizeof(long int));
-	printf("Size of a long long int : %d buyte(s) \n ", sizeof(long long int));
+{
+	printf("Size of a char: %d buyte(s) \n ", sizeof(char));
+	printf("Size of an int: %d buyte(s) \n ", sizeof(int));
+	printf("Size of a float: %d buyte(s) \n ", sizeof(float));
+	printf("Size of a long int: %d buyte(s) \n ", sizeof(long int));
+	printf("Size of a long long int: %d buyte(s) \n ", sizeof(long long int));
 	return (0);
 }
