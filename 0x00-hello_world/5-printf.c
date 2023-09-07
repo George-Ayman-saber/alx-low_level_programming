c8cef99371a6720e3d18632a4238e4d58e12490b
George <gogoayman767@gmail.com>
2023-09-07 14:56:00 -0700

    0-hello_world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+
 /**
  * main -start point
- * description :*/
+ * this is a c language pogram to print with
+ * printf
+ * and return always if sucess(0)
+*/
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
