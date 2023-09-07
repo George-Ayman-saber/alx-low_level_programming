9c5e0a022a34209b3ca24a896cf2c851558a7e46
George <gogoayman767@gmail.com>
2023-09-07 16:03:05 -0700

    0-hello_world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - startpoint
- * description :this is the progron to show the size of various type with
- * command sizeof
+ * main - Entry point
+ * Description :this is the progrom to show the size of various type with command sizeof
  * Return: 0 (success)
 */
+
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-	printf("Size of an float: %d byte(s)\n", sizeof(float));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
