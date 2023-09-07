8744365788f86cfa5fa3abda123bdba9ca2f2eb0
George <gogoayman767@gmail.com>
2023-09-07 15:14:33 -0700

    0-hello_world

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - startpoint
+ * description :this is the progron to show the size of various type with
+ * command sizeof
+ * Return: 0 (success)
+*/
+
+int main (void)
+{
+	int my_var = 22;
+	printf ("the size of my_var is %f buytes\n", sizeof(my_var));
+	return (0);
+}
