8c7ed4cdbe27aa40f92d270d46792a8c64a5da2b
George <gogoayman767@gmail.com>
2023-09-08 09:25:07 -0700

    variables_if_else_while

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - startpoint
+ * Description:this is a c program to do show
+ * if the rand number is nrgative or postive or zero
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	if (g > 0)
+	{
+		printf("g is positive\n");
+	}
+	else if (g < 0)
+	{
+		printf("g is negative \n");
+	}
+	else
+	{
+		printf("g is equal to zero\n");
+	}
+	return (0);
+}
