34b6307d35c1830b6a87a94f838e2e3202a02717
George <gogoayman767@gmail.com>
2023-09-08 17:08:43 -0700

    variables_if_else_while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - startpoint
+ * Description :This program will assign a random number
+ * to the variable n each time it is executed
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char x;
+
+	for (x = 'a' ; x <= 'z'; x++)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
