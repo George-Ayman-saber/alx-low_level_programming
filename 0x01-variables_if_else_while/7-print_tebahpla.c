29262fba622c9e3cc334ba67d18274f341c9065a
George <gogoayman767@gmail.com>
2023-09-08 18:03:42 -0700

    variables_if_else_while

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - startpoint
+ * Description :This is a c program that print all reversed alphabet
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char x;
+
+	for (x = 'z' ; x <= 'a'; x--)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
