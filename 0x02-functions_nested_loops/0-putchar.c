0bcfa063c0eb8bf3666050eaad529a91d655b8cd
George <gogoayman767@gmail.com>
2023-09-12 14:34:34 -0700

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - startpoint
+ * Description: this is a c program that print _putchar in new line
+ * Return : 0 (success)
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
