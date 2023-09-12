685da13f4f4302a7849c121ff14785a96380b6b8
George <gogoayman767@gmail.com>
2023-09-12 16:48:13 -0700

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - startpoint
+ * description: i call the function
+ * return: 0(success)
+*/
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,2 +1,20 @@
 #include "main.h"
+#include <stdio.h>
 
+/**
+ * print_alphabet_x10 - startpoint
+ * description: this is a c program that print all alphabet x10
+*/
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char j;
+	
+	for (i = 0;i <= 10; i++ )
+	{
+		for (j = 'a';j <= 'z'; j++)
+			_putchar(j);
+		_putchar('\n');
+	}
+}
