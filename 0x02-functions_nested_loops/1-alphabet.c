a081611cf68e5130775643c7528548c7d42da870
George <gogoayman767@gmail.com>
2023-09-12 16:14:12 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,17 @@
-#
+#include "main.h"
+
+/**
+ * main - startpoint
+ * Description : this is a c program that print alphabet
+ * Return: 0(success)
+*/
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+	return (0);
+}
