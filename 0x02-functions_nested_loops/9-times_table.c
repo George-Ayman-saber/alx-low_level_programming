38d1d42ba915a5988e2d0b4a05c76776379f9eae
George <gogoayman767@gmail.com>
2023-09-12 19:01:02 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,13 +5,15 @@ void times_table(void)
 {
 	int i, j, k, l;
 
-	for (i = 0; i <= 9, i++)
-		for (j = 0; j <= 9, j++)
+	for (i = 0; i <= 9; i++)
+		for (j = 0; j <= 9; j++)
 		{
-			for (k = j + 1; k <= 9, k++)
+			for (k = j + 1; k <= 9; k++)
 			{
 				l = k * j;
 				_putchar(l + '0');
+				_putchar(',');
+				_putchar(' ');
 			}
 			_putchar('\n');
 		}
