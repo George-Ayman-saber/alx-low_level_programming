d8c4d1c9429bab47a63ac4357565078a29e31f5f
George <gogoayman767@gmail.com>
2023-09-12 19:35:29 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,19 +1,30 @@
 #include "main.h"
 
-
-void times_table(void)
-{
+void times_table(void) {
 	int i, j, k;
-
+	
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 1; j <= 9; j++)
+		for (j = 0; j <= 9; j++)
 		{
-			k = i * j;
+			k = i * j
+			if (j != 0)
+			{
+				_putchar(44);  // comma
+				_putchar(32);  // spac
+			}
+			if (k < 10)
+			{
+				if (j != 10)
+					_putchar(' ');  // extra space for alignment
+			}
 			_putchar(k + '0');
-			_putchar(44);
-			_putchar(32);
 		}
-	_putchar('\n');
+		else 
+		{
+			_putchar((k / 10) + '0');
+			_putchar((k % 10) + '0');
+		}
 	}
+	_putchar('\n');
 }
