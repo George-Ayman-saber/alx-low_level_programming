93e84fd5f4c023823dae863d8e14692b51411a10
George <gogoayman767@gmail.com>
2023-09-13 18:57:39 -0700

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - startpoint
+ * description : print the sum of all numbers the divisable by 3 or 5
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int x, sum;
+
+	for (x = 1; x = 10; x++)
+	{
+		if ((x % 3 == 0) || (x % 5 == 0))
+			sum = sum + x;
+	}
+	printf("%d", sum);
+	return (0);
+}
