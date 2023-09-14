7860843dc6fad14ab2d053b4d12d9c51b8188ed1
George <gogoayman767@gmail.com>
2023-09-13 18:59:55 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,11 +10,11 @@ int main(void)
 {
 	int x, sum;
 
-	for (x = 1; x = 10; x++)
+	for (x = 1; x <= 10; x++)
 	{
 		if ((x % 3 == 0) || (x % 5 == 0))
 			sum = sum + x;
 	}
-	printf("%d", sum);
+	printf("%d \n", sum);
 	return (0);
 }
