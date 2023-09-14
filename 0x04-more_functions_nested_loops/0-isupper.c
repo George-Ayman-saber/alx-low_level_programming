8ef3612bed63bf026763e79a47243e411a824949
George <gogoayman767@gmail.com>
2023-09-14 15:21:58 -0700

    more_functions_nested_loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
+#include "main.h"
+#include <ctype.h>
 
 /**
  * main - startpoint
  * description: this is a c program the chaeck if c isupper or not
  * Return: 1 (success) 0 (failure)
+ * @c: is the variable the enter to check
 */
 
-int main(void)
+int _isupper(char c)
 {
-	char c;
-
 	if (isupper(c))
 		return (1);
 	else
-		return (0)
+		return (0);
 }
