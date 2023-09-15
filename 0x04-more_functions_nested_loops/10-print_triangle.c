ee259c0f4c8d0067e2c3f9e91f244aef354770e3
George <gogoayman767@gmail.com>
2023-09-14 17:22:31 -0700

    more_functions_nested_loops

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -17,9 +17,9 @@ void print_triangle(int size)
 		for (i = 0; i < size; i++)
 		{
 			for (j = 0; j < size - i; j++)
-					putchar(' ');
+				putchar(' ');
 			for (k = 0; k <= i; k++)
-					putchar('#');
+				putchar('#');
 			putchar('\n');
 		}
 	}
