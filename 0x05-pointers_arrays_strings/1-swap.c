6107d02f4ff5f16ce35e32a7b4e60dc2b0645986
George <gogoayman767@gmail.com>
2023-09-18 15:26:38 -0700

    pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * swap_int - startpoint
+ * this is a c program that exchange the velue of b to a and a to b
+ * @a:this is the  variable that enter in this is
+ * the address of variable in program
+ * @b:this is the  variable that enter in this is
+ *  the address of variable in program6
+*/
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a ;
+	*a = *b;
+	*b = temp;
+}
