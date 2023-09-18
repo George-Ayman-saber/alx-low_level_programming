7b7f4dbd5e98fd06813362db4a81a26203356d72
George <gogoayman767@gmail.com>
2023-09-18 09:20:10 -0700

    pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *  _strlen - startpoint
+ *  this is the c  program return len of str
+ *  Return: len
+ *  @str: this variable that stored the string and put it in my function
+*/
+
+int _strlen(char *str)
+{
+	int len;
+
+	len = sizeof(str);
+	return (len);
+}
