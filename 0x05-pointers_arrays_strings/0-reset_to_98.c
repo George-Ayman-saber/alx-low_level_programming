0f04f9eeeaedc48a642d0923a50b90c51b9dfa76
George <gogoayman767@gmail.com>
2023-09-18 08:43:16 -0700

    pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - startpoint
+ * this is a c program that change reset n to 98
+ * Return: *p =98 to change n
+ * @p : this is the variable that take address to change the value exist in address
+*/
+
+int reset_to_98.c(int *p)
+{
+	return (*p = 98);
+}
