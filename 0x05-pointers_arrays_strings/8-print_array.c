5bd898ff6bb4c2d80fed16a84785f2d31b9df4c8
George <gogoayman767@gmail.com>
2023-09-18 19:09:22 -0700

    pointers_arrays_strings

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,16 +1,23 @@
 #include "main.h"
 
-/***/
+/**
+ * print_array - start point
+ * this is a c program that Write a function that prints n
+ * elements of an array of integers, followed by a new line.
+ * @a: this is a var of arr that stored
+ * @n: this is the number that the arra contain arr[n]
+*/
 
 void print_array(int *a, int n)
 {
 	int i;
 
 	for (i = 0; i < n; i++)
-	{	if (i == n - 1)
-			printf("%d",a[i]);
+	{
+		if (i == n - 1)
+			printf("%d", a[i]);
 		else
-			printf("%d, ",a[i]);
+			printf("%d, ", a[i]);
 	}
 	printf("\n");
 }
