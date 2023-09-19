cc6f11606af33192559ada0c37742f0f603455e9
George <gogoayman767@gmail.com>
2023-09-18 18:34:44 -0700

    pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/***/
+
+void puts_half(char *str)
+{
+	int i = 0;
+	int mid;
+	int j;
+
+	for (i = 0; str[i] != '\0'; ++i)
+		;
+	if (i % 2 != 0)
+		mid = (i - 1) / 2;
+	else
+		mid = i / 2;
+	for (j = 0; j <= i; j++	)
+		_putchar(str[j]);
+	_putchar('\n')
