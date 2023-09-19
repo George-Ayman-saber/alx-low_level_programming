2ff476dea4796994e26d6f5a1e5310136388ed85
George <gogoayman767@gmail.com>
2023-09-18 18:54:56 -0700

    pointers_arrays_strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,16 +12,11 @@
 void puts2(char *str)
 {
 	int i = 0;
-	int j = 0;
 
 	for (i = 0; str[i] != '\0'; i++)
-		;
-	for (j = 0; j <= i; j++)
 	{
-		if (j % 2 == 0)
-			_putchar(str[j]);
-		else
-			continue;
+		if (i % 2 == 0)
+			_putchar(str[i]);
 	}
 	_putchar('\n');
 }
