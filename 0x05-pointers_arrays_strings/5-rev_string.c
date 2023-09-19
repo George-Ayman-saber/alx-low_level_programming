42c12e6db2b2c3871bda993828aec53a1d888499
George <gogoayman767@gmail.com>
2023-09-18 17:30:30 -0700

    pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rev_string - startpoint
+ * this is a c program to write a function that prints a string
+ * in reverse, followed by a new line
+ * @s: this is a var stored a addres of s
+*/
+
+void rev_string(char *s);
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	while (i--)
+        {
+		 _putchar(s[i]);
+	}
+	_putchar('\n');
+}
