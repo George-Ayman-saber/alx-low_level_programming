24298a6f3e3777619ee64243bbc8d30f36830fe6
George <gogoayman767@gmail.com>
2023-09-20 09:47:49 -0700

    pointers_arrays_strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,21 +1,25 @@
 #include "main.h"
 
 /**
- * _strncat -startpoint
+ * _strncat - startpoint
  * this is a c program that do a function that concatenates two strings.
- * return: @dest
+ * Return: dest
  * @dest: this is a var need to cp content in src to it
  * @src: thisis the var tha cp content from it
-*/
+ * @n: this number of char to stop on it*/
 
 char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0;
 	int j = 0;
 
-	while (dest[i])
-		i++;
+	for (i = 0; dest[i]; i++)
+		;
 	for (j = 0; src[j]; j++)
-		dest[j + i ] = src [j];
+	{
+		dest[j + i] = src[j];
+		if (j == n)
+			break;
+	}
 	return (dest);
 }
