5c25ff7d1487f284aad32959b6ec628cb0fa3c20
George <gogoayman767@gmail.com>
2023-09-20 15:16:05 -0700

    pointers_arrays_strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,16 +11,17 @@
 int _strcmp(char *s1, char *s2)
 {
 	int equal = 0;
+	int i = 0;
 
-	while (*s1)
+	for (i = 0; *s1[i] != '\0'; i++)
 	{
-		if (*s1 != *s2)
+		if (s1[i] != s2[i])
 		{
-			equal = ((int)*s1 - 48) - ((int)*s2 - 48);
+			equal = (s1[i] - s2[i]);
 			break;
 		}
-		s1++;
-		s2++;
+		else
+			continue;
 	}
 	return (equal);
 }
