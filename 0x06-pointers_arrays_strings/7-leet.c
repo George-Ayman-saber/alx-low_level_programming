d899e59f2d91085cc97f2b812d2eb9711b40f184
George <gogoayman767@gmail.com>
2023-09-21 17:13:28 -0700

    pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * leet - startpoint
+ * this is a function that do  encodes a string into 1337.
+ * @c: this is the var stored the content do encode
+ * Return: cp
+*/
+
+char *leet(char *c)
+{
+	unsigned int i;
+	char keys[] = {'A', 'E', 'O', 'T', 'L'};
+	char values[] = {4, 3, 0, 7, 1 };
+	char *cp = c;
+
+	while (*c)
+	{
+		for (i = 0; i < sizeof(keys) / sizeof(char); i++)
+		{
+			if (*c == keys[i] || *c == keys[i] + 32)
+			{
+				*c = 48 + value[i];
+			}
+		}
+		c++;
+	}
+	return (cp);
+}
