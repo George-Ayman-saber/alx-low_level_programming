6539359f27c6faacb4d720e1c81283491b876777
George-Ayman-saber <gogoayman767@gmail.com>
2023-09-28 16:43:37 -0700

    -pointers_arrays_strings

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _strlen_recursion - find the length of a string
+* @s: pointer to the string to check
+* Return: void
+*/
+
+int _strlen_recursion(char *s)
+{
+	int i;
+
+	if (*s > '\0')
+	{
+		i = _strlen_recursion(s + 1) + 1;
+	}
+	return (i);
+}
