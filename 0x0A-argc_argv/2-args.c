e627e8a5e0f35c7629676b4e1f7455b381fc7baf
George-Ayman-saber <gogoayman767@gmail.com>
2023-10-02 15:29:04 -0700

    argc_argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main -startpoint
+ * this is a c function that do a program that
+ * prints all arguments it receives.
+ * @argc: this is the size of argc
+ * @argv: this is the array
+ * return:0
+*/
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	(void)argc;
+	while (i < n)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
