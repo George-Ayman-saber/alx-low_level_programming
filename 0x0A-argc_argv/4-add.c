9703275fceef3f3ec6067b02be297a1dc87ade27
George-Ayman-saber <gogoayman767@gmail.com>
2023-10-02 16:09:11 -0700

    argc_argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -16,11 +16,9 @@ int main(int argc, char *argv[])
 	while (--argc)
 	{
 		for (c = argv[argc]; *c; c++)
-		{
 			if (*c < '0' || *c > '9')
 				return (printf("Error\n"), 1);
-			add += atoi(argv[argc]);
-		}
+		add += atoi(argv[argc]);	
 	}
 	printf("%d\n", add);
 	return (0);
