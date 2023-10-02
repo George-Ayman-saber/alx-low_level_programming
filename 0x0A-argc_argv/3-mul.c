2c3120b3d9f7e88d3051616ca1f55de7992331e1
George-Ayman-saber <gogoayman767@gmail.com>
2023-10-02 15:43:35 -0700

    argc_argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * main -startpoint
+ *this is a c program that multiplies two numbers.
+ * @argc: this is a size of array
+ * @argv: this is an array
+ * Return: 0 or 1 if failed
+*/
+
+int main(int argc, char *argv[])
+{
+	int i = 1;
+
+	if (argc <= 2)
+	{
+		printf ("Error\n");
+		return (1);
+	}
+	else
+	{
+		int mul = 1;
+		while (i < argc)
+		{
+			mul *= argv[i];
+			i++;
+		}
+		return (mul);
+	}
