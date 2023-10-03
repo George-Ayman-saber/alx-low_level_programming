5f2e23d96a004ebcf7a1a93ffea7af10f5e33ee1
George-Ayman-saber <gogoayman767@gmail.com>
2023-10-03 15:18:44 -0700

    pointers_arrays_strings

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,32 @@
+#ifndef MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#define MAIN_H
+int _putchar(char c);
+
+
+
+
+
+
+char *create_array(unsigned int size, char c);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
