b98232bd808ca09b8b5ae65bd5aed21da3585f24
George-Ayman-saber <gogoayman767@gmail.com>
2023-10-03 15:36:32 -0700

    pointers_arrays_strings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/***/
+
+char *_strdup(char *str)
+{
+	char *n = malloc(sizeof(str));
+
+	if ( str == '\0')
+		return (0);
+	for (i = 0; str[i] != '\0'; i++)
+		n[i] = str[i];
+	return (n);
+}
