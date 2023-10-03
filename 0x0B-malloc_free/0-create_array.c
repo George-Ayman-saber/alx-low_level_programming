b2d05cb5ca0efc2e9af06c8f4928186f9858d0db
George-Ayman-saber <gogoayman767@gmail.com>
2023-10-03 15:26:04 -0700

    pointers_arrays_strings

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,9 +13,9 @@ char *create_array(unsigned int size, char c)
 {
 	char *n = malloc(size);
 
-	if ( n == 0 || size == 0)
-		return (NULL);
-	while(--size)
+	if (n == 0 || size == 0)
+		return (0);
+	while (--size)
 		n[size] = c;
 	return (n);
 }
