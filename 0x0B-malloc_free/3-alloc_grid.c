d77ea5a19f8bbf1de3cc204a4c31ede57e906634
George-Ayman-saber <gogoayman767@gmail.com>
2023-10-05 09:02:45 -0700

    malloc and free

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * alloc_grid - startopoint
+ * this is a function that returns a pointer
+ * to a 2 dimensional array of integers
+ * @width: this is the width
+ * @height: this is a height of the dime.
+ * Return: 0
+*/
+
+int **alloc_grid(int width, int height)
+{
+	int i = 0;
+	int j = 0;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	int **m;
+
+	m = malloc((height) * sizeof(*m));
+	for (; i < height; i++)
+	{
+		m[i] = malloc(width * sizeof(**m));
+		if (m[i] == 0)
+		{
+			while (i--)
+				free(m[i]);
+			free(tab);
+			return (NULL);
+		}
+		else
+		{
+			for (; j < width; j++)
+			{
+				m[i][j] = 0;
+			}
+		}
+	return (m);
+	}
+}
