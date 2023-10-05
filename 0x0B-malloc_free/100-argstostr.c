1655f73213ff63d6c256f82c7f9c840d5ac188d2
George-Ayman-saber <gogoayman767@gmail.com>
2023-10-05 15:06:18 -0700

    malloc and free

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,5 +1,21 @@
 #include "main.h"
-int _strlen(char *s);
+
+/**
+ *  _strlen - startpoint
+ *  this is the c  program return len of str
+ *  Return: counter
+ *  @s: this variable that stored the string and put it in my function
+*/
+
+int _strlen(char *s)
+{
+	int counter;
+
+	for (counter = 0; *s != '\0'; s++)
+		++counter;
+	return (counter);
+}
+
 /**
  * argstostr - startpoint
  * this is a c function that concatenates all the arguments of your program.
