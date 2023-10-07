5bdb2f1b09157654292a9f84abc1d477b0409689
George-Ayman-saber <gogoayman767@gmail.com>
2023-10-06 17:18:19 -0700

    more malloc and free

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _calloc - startpoint
+ * this is a c  function that allocates memory for an array, using malloc.
+ * like clloc with default 0
+ * @nmemb: this is the number of element
+ * @size: this is a sizr of data type
+ * Return: 0
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *n;
+	int i;
+
+	n = malloc(nmemb * size);
+	if (n == NULL || nmemb == 0 || size == 0)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		m[i] = 0;
+	return (m);
+}
