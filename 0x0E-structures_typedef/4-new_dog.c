a33459864ba503570d79b9b33bf44a907d9ace43
George-Ayman-saber <gogoayman767@gmail.com>
2023-10-10 14:39:37 -0700

    structures_typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,4 +1,42 @@
 #include "dog.h"
+#include <stdlib>
+
+/**
+ *  _strlen - startpoint
+ *  this is the c  program return len of str
+ *  Return: counter
+ *  @s: this variable that stored the string and put it in my function
+*/
+
+int _strlen(char *s)
+{
+	int counter;
+
+	for (counter = 0; *s != '\0'; s++)
+		++counter;
+	return (counter);
+}
+
+/**
+ * *_strcpy -startpoint
+ * this is a c program that do a function that copies the string pointed to by
+ * src, including the terminating null byte (\0),
+ * to the buffer pointed to by dest.
+ * Return: the pointer to dest
+ * @dest: this arr that src cp to it
+ * @src: this arr that cp from it to dest
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = -1;
+
+	do {
+		i++;
+		dest[i] = src[i];
+	} while (src[i] != '\0');
+	return (dest);
+}
 
 /**
  * dog_t *new_dog - startpoint
@@ -11,12 +49,10 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	if (new_dog)
+	dog_t *dog
+
+	if (!name ||age < 0 || !owner)
+		return (NULL);
+	dog->name = malloc(sizeof(char)* (_stlen(name) + 1));
+	if((*dog).name
 	{
-		new_dog->name = name;
-		new_dog->age = age;
-		new_dog->owner = owner;
-		return (0);
-	}
-	return (NULL);
-}
