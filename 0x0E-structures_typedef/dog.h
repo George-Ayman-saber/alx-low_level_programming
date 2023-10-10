e1df4b069e2fded4848f92fe0acf96cdb65b61fb
George-Ayman-saber <gogoayman767@gmail.com>
2023-10-10 13:31:28 -0700

    structures_typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,12 @@
-#ifndef _DOG_H
-#define _DOG_H
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct dog - a dog struct 
  * @name: dog s name
  * @age: the dog s age
  * @owner: owner s name
+ * Description: just a lone dog struct in a bd kitty world
 */
 
 struct dog
@@ -13,5 +14,6 @@ struct dog
 	char *name;
 	char *owner;
 	float age;
-};
+};i
+
 #endif
