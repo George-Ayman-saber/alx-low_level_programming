042f65d90823885ed296347f04693abe703c0626
George-Ayman-saber <gogoayman767@gmail.com>
2023-10-10 14:58:26 -0700

    structures_typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -30,7 +30,8 @@ int _strlen(char *s)
 char *_strcpy(char *dest, char *src)
 {
 	int i;
-	for (i = 0; src[i] !='\0'; i++)
+
+	for (i = 0; src[i]; i++)
 		dest[i] = src[i];
 	dest[i] = '\0';
 	return (dest);
