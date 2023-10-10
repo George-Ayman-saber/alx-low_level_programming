0217cfdb0fb259bc10e54ad153a8919ac565d0a2
George-Ayman-saber <gogoayman767@gmail.com>
2023-10-10 14:50:37 -0700

    structures_typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -50,12 +50,22 @@ char *_strcpy(char *dest, char *src)
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog
-
+	
 	if (!name || age < 0 || !owner)
+		return (NULL);
+	dog = (dog_t *) malloc(sizeof(dog_t));
+	if (dog==NULL)
 		return (NULL);
 	dog->name = malloc(sizeof(char) * (_stlen(name) + 1));
 	if ((*dog).name == NULL)
 	{
+		free(dog);
+		return (NULL);
+	}
+	dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if ((*dog).owner == NULL)
+	{
+		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
