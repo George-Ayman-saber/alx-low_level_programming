8fee414cd6cdf87596c208680871d9c0b01708c2
George-Ayman-saber <gogoayman767@gmail.com>
2023-10-11 13:56:23 -0700

    structures_typedef

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - this is  returns the sum of a and b.
+ * @a: this is first number
+ * @b: this is second no.
+ * Return: a + b
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ *op_sub - this is  returns the difference of a and b
+ * @a: this is first number
+ * @b: this is second no.
+ * Return: a - b
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - this is  returns the product of a and b.
+ * @a: this is first number
+ * @b: this is second no.
+ * Return: a * b
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - this is returns the result of the division of a by b
+ * @a: this is first number
+ * @b: this is second no.
+ * Return: a / b
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - this is returns the remainder of the division of a by b
+ * @a: this is first number
+ * @b: this is second no.
+ * Return: a%b
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
