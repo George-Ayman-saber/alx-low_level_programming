687863160640267205474058ea4ad19b03e21234
George-Ayman-saber <gogoayman767@gmail.com>
2023-10-12 08:41:34 -0700

    varadic functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,13 +9,11 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list ptr;
-
-	va_start(ptr, n);
+	int i, sum = 0;
 
 	if (n == 0)
 		return (0);
-	int i, sum = 0;
-
+	va_start(ptr, n);
 	for (i = 0; i < n; i++)
 		sum += va_arg(ptr, int);
 	va_end(ptr);
